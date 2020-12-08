54dd04108896b7ab2ec7bdf0d9e79e96438335a4
Igor Dedkov <igordeadkov@gmail.com>
2020-12-08 02:11:47 -0800

    Add Remove from Cart Functionality

--- IDRMDesktopUI/ViewModels/SalesViewModel.cs
+++ IDRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -66,6 +66,18 @@ namespace IDRMDesktopUI.ViewModels
             }
         }
 
+        private CartItemDisplayModel _selectedCartItem;
+        public CartItemDisplayModel SelectedCartItem
+        {
+            get { return _selectedCartItem; }
+            set
+            {
+                _selectedCartItem = value;
+                NotifyOfPropertyChange(() => SelectedCartItem);
+                NotifyOfPropertyChange(() => CanRemoveFromCart);
+            }
+        }
+
 
         private BindingList<CartItemDisplayModel> _cart = new BindingList<CartItemDisplayModel>();
 
@@ -199,6 +211,10 @@ namespace IDRMDesktopUI.ViewModels
                 bool output = false;
 
                 // Make sure something is selected
+                if (SelectedCartItem != null && SelectedCartItem?.Product.QuantityInStock > 0)
+                {
+                    output = true;
+                }
 
                 return output;
             }
@@ -206,6 +222,19 @@ namespace IDRMDesktopUI.ViewModels
 
         public void RemoveFromCart()
         {
+            
+            
+            SelectedCartItem.Product.QuantityInStock += 1;
+
+            if (SelectedCartItem.QuantityInCart > 1)
+            {
+                SelectedCartItem.QuantityInCart -= 1;                
+            }
+            else
+            {                
+                Cart.Remove(SelectedCartItem);
+            }
+
             NotifyOfPropertyChange(() => SubTotal);
             NotifyOfPropertyChange(() => Tax);
             NotifyOfPropertyChange(() => Total);
