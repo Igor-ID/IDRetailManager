676cc5e3f31988ae1acf0903df653430a3b2caad
Igor Dedkov <igordeadkov@gmail.com>
2021-01-14 22:29:27 -0800

    Add Dialog Box and Check Permissions
    Checks to be sure a user has access and alerts them if they do not.

--- IDRMDesktopUI/ViewModels/SalesViewModel.cs
+++ IDRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -7,9 +7,11 @@ using IDRMDesktopUILibrary.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Dynamic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace IDRMDesktopUI.ViewModels
 {
@@ -19,19 +21,47 @@ namespace IDRMDesktopUI.ViewModels
         private IConfigHelper _confighelper;
         private ISaleEndpoint _saleEndpoint;
         private IMapper _mapper;
+        private readonly StatusInfoViewModel _status;
+        private readonly IWindowManager _window;
 
-        public SalesViewModel(IProductEndpoint productEndpoint, IConfigHelper confighelper, ISaleEndpoint saleEndpoint, IMapper mapper)
+        public SalesViewModel(IProductEndpoint productEndpoint, IConfigHelper confighelper, ISaleEndpoint saleEndpoint, IMapper mapper, 
+            StatusInfoViewModel status, IWindowManager window)
         {
             _productEndpoint = productEndpoint;
             _confighelper = confighelper;
             _saleEndpoint = saleEndpoint;
             _mapper = mapper;
+            _status = status;
+            _window = window;
         }
 
         protected override async void OnViewLoaded(object view)
         {
             base.OnViewLoaded(view);
-            await LoadProducts();
+            try
+            {
+                await LoadProducts();
+            }
+            catch (Exception ex)
+            {
+                dynamic settings = new ExpandoObject();
+                settings.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                settings.ResizeMode = ResizeMode.NoResize;
+                settings.Title = "System Error";
+                
+                if (ex.Message == "Unauthorized")
+                {
+                    _status.UpdateMesage("Unauthorized Access", "You do not have permission to interact with the Sales Form.");
+                    _window.ShowDialog(_status, null, settings);
+                }
+                else
+                {
+                    _status.UpdateMesage("Fatal Exception", ex.Message);
+                    _window.ShowDialog(_status, null, settings);
+                }
+
+                TryClose();
+            }
         }
 
         private async Task LoadProducts()
@@ -272,6 +302,7 @@ namespace IDRMDesktopUI.ViewModels
 
         public async Task CheckOut()
         {
+            // Create 
             SaleModel sale = new SaleModel();
 
             foreach (var item in Cart)
